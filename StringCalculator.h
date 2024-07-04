421dca1b8a97b7787250e5dcbb9895cd01b625f1
Shriya Tatachar <62716775+TatShri@users.noreply.github.com>
2024-07-04 12:48:52 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,9 +1,13 @@
+// StringCalculator.h
+
 #ifndef STRINGCALCULATOR_H
 #define STRINGCALCULATOR_H
 
-#include <stdexcept>
 #include <string>
 
-int StringCalculator (const std::string& input);
+class StringCalculator {
+public:
+    int add(const std::string& input);
+};
 
 #endif // STRINGCALCULATOR_H
