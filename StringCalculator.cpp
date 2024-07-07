6b8aacc41107b27fca345b64bf1d553394bb0089
Shriya Tatachar <62716775+TatShri@users.noreply.github.com>
2024-07-07 20:11:47 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -3,11 +3,10 @@
 #include "StringCalculator.h"
 
 int StringCalculator::add(const std::string& input) {
-    if (input.empty()) {
+    if (input.empty() || input == "0") {
         return 0;
     }
     else {
         return -1;
     }
 }
-
