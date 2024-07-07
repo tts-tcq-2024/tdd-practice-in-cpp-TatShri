dc4af5574ed0bbef66f57382a65d629a6a8493f4
Shriya Tatachar <62716775+TatShri@users.noreply.github.com>
2024-07-07 20:25:17 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include "StringCalculator.h"
 
-int StringCalculator::add(const std::string& input) {
-    if (input.empty() || input == "0") {
-        return 0;
+class StringCalculator {
+public:
+    int add(const std::string& input) {
+        return isEmptyOrZero(input) * computeSum(input);
     }
-    else {
-        return -1;
+
+private:
+    int isEmptyOrZero(const std::string& input) {
+        return input.empty() || input == "0";
     }
-}
+
+    int computeSum(const std::string& input) {
+        std::stringstream ss(input);
+        int sum = 0;
+        std::string number;
+
+        std::getline(ss, number, ',');
+        sum += std::stoi(number);
+
+        std::getline(ss, number, ',');
+        sum += std::stoi(number);
+
+        return sum;
+    }
+};
