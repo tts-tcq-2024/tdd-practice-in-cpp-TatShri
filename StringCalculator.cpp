16cb78832afe77c5f3822a7b741aa11c22be595c
Shriya Tatachar <62716775+TatShri@users.noreply.github.com>
2024-07-14 20:46:20 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -4,61 +4,61 @@
 #include <algorithm>
 #include <vector>
 
-bool StringCalculator::isInputEmptyOrZero(const std::string& input) {
-    return input.empty() || input == "0";
-}
-
-void StringCalculator::checkForNegative(int num) {
-    if (num < 0) {
-        throw std::runtime_error("negatives not allowed");
+bool isInputEmptyOrZero(const std::string& input) {
+        return input.empty() || input == "0";
     }
-}
 
-bool StringCalculator::isNumberLessThanOrEqualTo1000(int num) {
-    return num <= 1000;
-}
-
-std::string StringCalculator::extractCustomDelimiter(const std::string& input) {
-    std::string delimiter;
-    if (input.find("//") == 0) {
-        size_t delimiter_start = 2;
-        size_t delimiter_end = input.find("\n", delimiter_start);
-        if (delimiter_end != std::string::npos) {
-            delimiter = input.substr(delimiter_start, delimiter_end - delimiter_start);
+void checkForNegative(int num) {
+        if (num < 0) {
+            throw std::runtime_error("negatives not allowed");
         }
     }
-    return delimiter;
-}
 
-void StringCalculator::processNumberAndUpdateSum(int& sum, const std::string& number) {
-    int num = std::stoi(number);
-    checkForNegative(num);
-    if (isNumberLessThanOrEqualTo1000(num)) {
-        sum += num;
-    }
-}
-
-int StringCalculator::sumNumbers(const std::string& numbers, const std::string& delimiter) {
-    int sum = 0;
-    std::stringstream ss(numbers);
-    std::string number;
-
-    while (std::getline(ss, number, delimiter[0])) {
-        processNumberAndUpdateSum(sum, number);
+bool isNumberLessThanOrEqualTo1000(int num) {
+        return num <= 1000;
     }
 
-    return sum;
-}
+std::string extractCustomDelimiter(const std::string& input) {
+        std::string delimiter;
+        if (input.find("//") == 0) {
+            size_t delimiter_start = 2;
+            size_t delimiter_end = input.find("\n", delimiter_start);
+            if (delimiter_end != std::string::npos) {
+                delimiter = input.substr(delimiter_start, delimiter_end - delimiter_start);
+            }
+        }
+        return delimiter;
+    }
 
-int StringCalculator::handleDefaultDelimiters(const std::string& input) {
-    const std::vector<char> defaultDelimiters = { ',', '\n' };
-    for (char delimiter : defaultDelimiters) {
-        if (input.find(delimiter) != std::string::npos) {
-            return sumNumbers(input, std::string(1, delimiter));
+void processNumberAndUpdateSum(int& sum, const std::string& number) {
+        int num = std::stoi(number);
+        checkForNegative(num);
+        if (isNumberLessThanOrEqualTo1000(num)) {
+            sum += num;
         }
     }
-    return std::stoi(input);
-}
+
+int sumNumbers(const std::string& numbers, const std::string& delimiter) {
+        int sum = 0;
+        std::stringstream ss(numbers);
+        std::string number;
+
+        while (std::getline(ss, number, delimiter[0])) {
+            processNumberAndUpdateSum(sum, number);
+        }
+
+        return sum;
+    }
+
+int handleDefaultDelimiters(const std::string& input) {
+        const std::vector<char> defaultDelimiters = { ',', '\n' };
+        for (char delimiter : defaultDelimiters) {
+            if (input.find(delimiter) != std::string::npos) {
+                return sumNumbers(input, std::string(1, delimiter));
+            }
+        }
+        return std::stoi(input);
+    }
 
 int StringCalculator::add(const std::string& input) {
     // Main logic of the add function
