c5fd6c884e21e5982b44bd825a028b86bfc850c7
Shriya Tatachar <62716775+TatShri@users.noreply.github.com>
2024-07-14 21:01:06 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -5,73 +5,77 @@
 #include <vector>
 
 bool isInputEmptyOrZero(const std::string& input) {
-        return input.empty() || input == "0";
-    }
+    return input.empty() || input == "0";
+}
 
 void checkForNegative(int num) {
-        if (num < 0) {
-            throw std::runtime_error("negatives not allowed");
-        }
+    if (num < 0) {
+        throw std::runtime_error("negatives not allowed");
     }
+}
 
 bool isNumberLessThanOrEqualTo1000(int num) {
-        return num <= 1000;
-    }
+    return num <= 1000;
+}
 
 std::string extractCustomDelimiter(const std::string& input) {
-        std::string delimiter;
-        if (input.find("//") == 0) {
-            size_t delimiter_start = 2;
-            size_t delimiter_end = input.find("\n", delimiter_start);
-            if (delimiter_end != std::string::npos) {
-                delimiter = input.substr(delimiter_start, delimiter_end - delimiter_start);
-            }
+    std::string delimiter;
+    if (input.find("//") == 0) {
+        size_t delimiter_start = 2;
+        size_t delimiter_end = input.find("\n", delimiter_start);
+        if (delimiter_end != std::string::npos) {
+            delimiter = input.substr(delimiter_start, delimiter_end - delimiter_start);
         }
-        return delimiter;
     }
+    return delimiter;
+}
 
 void processNumberAndUpdateSum(int& sum, const std::string& number) {
-        int num = std::stoi(number);
-        checkForNegative(num);
-        if (isNumberLessThanOrEqualTo1000(num)) {
-            sum += num;
+    int num = std::stoi(number);
+    checkForNegative(num);
+    if (isNumberLessThanOrEqualTo1000(num)) {
+        sum += num;
+    }
+}
+
+int sumNumbers(const std::string& numbers, const std::vector<std::string>& delimiters) {
+    int sum = 0;
+    std::stringstream ss(numbers);
+    std::string number;
+
+    while (std::getline(ss, number, delimiters[0][0])) {
+        std::stringstream sub_ss(number);
+        std::string sub_number;
+        while (std::getline(sub_ss, sub_number, delimiters[1][0])) {
+            processNumberAndUpdateSum(sum, sub_number);
         }
     }
 
-int sumNumbers(const std::string& numbers, const std::string& delimiter) {
-        int sum = 0;
-        std::stringstream ss(numbers);
-        std::string number;
-
-        while (std::getline(ss, number, delimiter[0])) {
-            processNumberAndUpdateSum(sum, number);
-        }
-
-        return sum;
-    }
+    return sum;
+}
 
 int handleDefaultDelimiters(const std::string& input) {
-        const std::vector<char> defaultDelimiters = { ',', '\n' };
-        for (char delimiter : defaultDelimiters) {
-            if (input.find(delimiter) != std::string::npos) {
-                return sumNumbers(input, std::string(1, delimiter));
-            }
+    const std::vector<std::string> defaultDelimiters = { ",", "\n" };
+
+    for (const std::string& delimiter : defaultDelimiters) {
+        if (input.find(delimiter) != std::string::npos) {
+            return sumNumbers(input, defaultDelimiters);
         }
-        return std::stoi(input);
     }
 
+    return std::stoi(input);
+}
+
 int StringCalculator::add(const std::string& input) {
-    // Main logic of the add function
-    if (isInputEmptyOrZero(input)) {
+    if (input.empty()) {
         return 0;
     }
 
     std::string customDelimiter = extractCustomDelimiter(input);
     if (!customDelimiter.empty()) {
         std::string numbersString = input.substr(input.find("\n") + 1);
-        return sumNumbers(numbersString, customDelimiter);
+        return sumNumbers(numbersString, { customDelimiter, "\n" });
     } else {
         return handleDefaultDelimiters(input);
     }
 }
-
